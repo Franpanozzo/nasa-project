8b0c522a712ac0fe2ddbb100c30911f1d0a95f82
Franpanozzo <francisco.panozzosf@gmail.com>
2022-04-02 19:07:58 -0300

    test launches GET

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  })
+})
+
+describe('Test POST /launch', () => {
+  test('It should respond with 200 success', () => {
+
+  })
+
+  test('It should catch missing require properties', () => {
+    
+  })
+
+  test('It should catch invalid dates', () => {
+
+  })
+})
